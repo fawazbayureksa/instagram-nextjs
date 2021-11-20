2a2a3f0ec60547fd21459ca8a89b62bfa4b5433a
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-11-20 19:47:03 +0800

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,30 @@
+
+import Image from "next/image";
+
 function Header() {
     return (
         <div>
-            <h1>Header</h1>
+            <div className="flex justify-between max-w-6xl">
+            {/* Kiri */}
+
+                <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
+                    <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain" />
+                </div>
+                <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+                    <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain" />
+                </div>
+        
+            {/* Tengah masukan cari */}
+            
+                <div>
+                    <input placeholder="Search" type="text" className="h-6 bg-gray-200 border-solid border-1 border-gray-200 text-sm"></input>
+                </div>
+
+
+            {/* Kanan */}
+            </div>
+
+
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex">
       <Head>
         <title>Instagram - Clone</title>
         <link rel="icon" href="/favicon.ico" />
@@ -18,16 +18,6 @@ export default function Home() {
       {/* Modul */}
 
 
-      <footer className="flex items-center justify-center w-full h-20 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Created By Fawwaz Bayureksa
-        </a>
-      </footer>
     </div>
   )
 }
