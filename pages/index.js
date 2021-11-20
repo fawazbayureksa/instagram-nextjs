66f6df26b2cccf0eee7c99ebb7e11bad57a02472
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-11-20 14:04:57 +0800

    commit mulai

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,18 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram - Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Hello World
-        </h1>
-      </main>
+      {/* Header */}
+
+
+      {/* Feed */}
+
+
+      {/* Modul */}
+
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
