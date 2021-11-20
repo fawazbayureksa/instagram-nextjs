bd2d4429edd526db1fa25363aed8558f72b2e487
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-11-20 14:33:12 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-
+import Head from 'next/head';
+import Header from '../components/Header';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -10,6 +10,7 @@ export default function Home() {
 
       {/* Header */}
 
+      <Header />
 
       {/* Feed */}
 
@@ -17,7 +18,7 @@ export default function Home() {
       {/* Modul */}
 
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-20 border-t">
         <a
           className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
