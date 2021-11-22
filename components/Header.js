c24f2979d96833dba95f2b7598697de78e7d4ee1
fawazbayureksa <fawwazbayureksa@gmail.com>
2021-11-22 20:45:16 +0800

    navbar kanan atas

--- components/Header.js
+++ components/Header.js
@@ -11,18 +11,18 @@ import {
 
 function Header() {
   return (
-    <div >
-      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+    <div className="shadow-sm bg-white sticky top-0 z-50">
+      <div className="flex justify-between max-w-4xl mx-5 lg:mx-auto">
             {/* Kiri */}
 
-            <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
+            <div className="relative hidden lg:inline-grid w-24 h-14 cursor-pointer">
             <Image
                 src="https://links.papareact.com/ocw"
                 layout="fill"
                 objectFit="contain"
             />
             </div>
-            <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer mt-8">
+            <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer mt-2">
             <Image
                 src="https://links.papareact.com/jjm"
                 layout="fill"
@@ -32,27 +32,27 @@ function Header() {
 
             {/* Tengah masukan cari */}
             <div className="max-w-xs">
-                <div className="relative mt-5 p-3 rounded-md">
+                <div className="relative px-3 py-3 rounded-md">
                     <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                     <SearchIcon className="h-5 w-5 text-gray-500" />
                     </div>
                     <input
                     placeholder="Search"
                     type="text"
-                    className=" bg-gray-50 block w-full pl-10 sm:text-sm rounded-md"
+                    className=" bg-gray-50 block w-full h-7 pl-10 sm:text-sm rounded-lg"
                     />
                 </div>
             </div>
 
             {/* Kanan Icon */}
-            <div className="flex items-center justify-end space-x-4 mt-2">
+            <div className="flex items-center justify-end space-x-4">
 
                 <HomeIcon className="navBtn"/>
 
                 <div className="relative navBtn">
                   <PaperAirplaneIcon className="navBtn rotate-45"/>
                       <div className="absolute -top-1 -right-2 text-xs w-5 h-5 text-white bg-red-500 rounded-full flex justify-center animate-pulse">
-                        3
+                        4
                       </div>
                 </div>
                 <PlusCircleIcon className=" navBtn"/>
